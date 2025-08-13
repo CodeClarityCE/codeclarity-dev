c11b2c2af57965b5617d0be327575c50000c6381
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-08-13 19:38:12 +0200

    Update Docker Compose scripts to replace js-license and js-vuln-finder plugins with license-finder and vuln-finder, and add php-sbom support

--- .cloud/scripts/build-debug.sh
+++ .cloud/scripts/build-debug.sh
@@ -11,12 +11,14 @@ docker compose -f ../docker/docker-compose.yaml \
 	-f ../../backend/services/dispatcher/.cloud/docker/docker-compose.debug.yaml \
 	-f ../../backend/plugins/codeql/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/plugins/codeql/.cloud/docker/docker-compose.debug.yaml \
-	-f ../../backend/plugins/js-license/.cloud/docker/docker-compose.yaml \
-	-f ../../backend/plugins/js-license/.cloud/docker/docker-compose.debug.yaml \
+	-f ../../backend/plugins/license-finder/.cloud/docker/docker-compose.yaml \
+	-f ../../backend/plugins/license-finder/.cloud/docker/docker-compose.debug.yaml \
 	-f ../../backend/plugins/js-patching/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/plugins/js-patching/.cloud/docker/docker-compose.debug.yaml \
 	-f ../../backend/plugins/js-sbom/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/plugins/js-sbom/.cloud/docker/docker-compose.debug.yaml \
-	-f ../../backend/plugins/js-vuln-finder/.cloud/docker/docker-compose.yaml \
-	-f ../../backend/plugins/js-vuln-finder/.cloud/docker/docker-compose.debug.yaml \
+	-f ../../backend/plugins/vuln-finder/.cloud/docker/docker-compose.yaml \
+	-f ../../backend/plugins/vuln-finder/.cloud/docker/docker-compose.debug.yaml \
+	-f ../../backend/plugins/php-sbom/.cloud/docker/docker-compose.yaml \
+	-f ../../backend/plugins/php-sbom/.cloud/docker/docker-compose.debug.yaml \
 	build $1
--- .cloud/scripts/up.sh
+++ .cloud/scripts/up.sh
@@ -6,8 +6,9 @@ docker compose -f ../docker/docker-compose.yaml \
 	-f ../../backend/services/downloader/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/services/dispatcher/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/plugins/codeql/.cloud/docker/docker-compose.yaml \
-	-f ../../backend/plugins/js-license/.cloud/docker/docker-compose.yaml \
+	-f ../../backend/plugins/license-finder/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/plugins/js-patching/.cloud/docker/docker-compose.yaml \
 	-f ../../backend/plugins/js-sbom/.cloud/docker/docker-compose.yaml \
-	-f ../../backend/plugins/js-vuln-finder/.cloud/docker/docker-compose.yaml \
+	-f ../../backend/plugins/vuln-finder/.cloud/docker/docker-compose.yaml \
+	-f ../../backend/plugins/php-sbom/.cloud/docker/docker-compose.yaml \
 	up -d $1
